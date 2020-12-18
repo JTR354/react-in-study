4012bb8b6504248496e7876b5e5ca05418e8ede5
mi <jtr354@foxmail.com>
2020-12-18 17:58:35 +0800

    init vikingship

---
+++ packages/vikingship/src/App.tsx
@@ -0,0 +1,16 @@
+
+function App() {
+  return (
+    <div className="App">
+      <h1>h1</h1>
+      <h3>h3</h3>
+      <h5>h5</h5>
+      <code>
+        a = 'b '
+      </code>
+      <a href="#">link a</a>
+    </div>
+  );
+}
+
+export default App;
